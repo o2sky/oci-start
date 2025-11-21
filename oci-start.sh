20a3f7d2562b531d4f45b36475dc9054aadc8c91
o2sky <162720477+o2sky@users.noreply.github.com>
2025-11-22 01:38:21 +0800

    Add files via upload

---
+++ oci-start.sh
@@ -0,0 +1,17 @@
+
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>503 Backend unavailable, connection timeout</title>
+  </head>
+  <body>
+    <h1>Error 503 Backend unavailable, connection timeout</h1>
+    <p>Backend unavailable, connection timeout</p>
+    <h3>Error 54113</h3>
+    <p>Details: cache-qpg120108-QPG 1763746639 2310315116</p>
+    <hr>
+    <p>Varnish cache server</p>
+  </body>
+</html>
